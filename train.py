04f5d66ba88232a2c45fcfac0460d7dd310e1b73
Ethanol <ethanol017217@gmail.com>
2025-06-03 16:36:30 +0800

    Add save and load in train

--- train.py
+++ train.py
@@ -43,7 +43,6 @@ def train(config):
     env = gym.make("osu_env/osu-v0",human_play_test=False)
     replay_buffer = ReplayBuffer(config["buffer_size"])
     actor = models.Actor(log_std_min=config["log_std_min"],log_std_max=config["log_std_max"]).to(device)
-    
     critic1 = models.Critic().to(device)
     critic2 = models.Critic().to(device)
     
@@ -53,13 +52,36 @@ def train(config):
     critic2_target.load_state_dict(critic2.state_dict())
     
     values_optimizer = optim.Adam(list(critic1.parameters()) + list(critic2.parameters()), lr=config["critic_lr"])
+    for param_group in values_optimizer.param_groups:
+        param_group['lr'] = config["critic_lr"]
     policy_optimizer = optim.Adam(list(actor.parameters()), lr=config["actor_lr"])
+    for param_group in policy_optimizer.param_groups:
+        param_group['lr'] = config["actor_lr"]
     
+    if config["load_model"]:
+        checkpoint = torch.load(config["load_path"], map_location=device)
+        actor.load_state_dict(checkpoint["actor"])
+        critic1.load_state_dict(checkpoint["critic1"])
+        critic2.load_state_dict(checkpoint["critic2"])
+        critic1_target.load_state_dict(checkpoint["critic1_target"])
+        critic2_target.load_state_dict(checkpoint["critic2_target"])
+        values_optimizer.load_state_dict(checkpoint["values_optimizer"])
+        policy_optimizer.load_state_dict(checkpoint["policy_optimizer"])
+        config["alpha_cont"] = checkpoint["alpha_cont"]
+        config["alpha_disc"] = checkpoint["alpha_disc"]
+        
     if config["alpha_autotune"]:
         log_alpha_cont = torch.tensor(np.log(config["alpha_cont"]), requires_grad=True).to(device)
         log_alpha_disc = torch.tensor(np.log(config["alpha_disc"]), requires_grad=True).to(device)
         alpha_cont_optimizer = optim.Adam([log_alpha_cont], lr=config["alpha_autotune_lr"])
         alpha_disc_optimizer = optim.Adam([log_alpha_disc], lr=config["alpha_autotune_lr"])
+        if config["load_model"]:
+            alpha_cont_optimizer.load_state_dict(checkpoint["alpha_cont_optimizer"])
+            for param_group in alpha_cont_optimizer.param_groups:
+                param_group['lr'] = config["alpha_autotune_lr"]
+            alpha_disc_optimizer.load_state_dict(checkpoint["alpha_disc_optimizer"])
+            for param_group in alpha_disc_optimizer.param_groups:
+                param_group['lr'] = config["alpha_autotune_lr"]
         alpha_cont = log_alpha_cont.exp().detach()
         alpha_disc = log_alpha_disc.exp().detach()
         alpha_cont_target = torch.tensor(-2.0).to(device) # -2 is -(continous dim)
@@ -144,7 +166,20 @@ def train(config):
                         target_param.data.copy_(config["tau"] * param.data + (1 - config["tau"]) * target_param.data)
                     for param, target_param in zip(critic2.parameters(), critic2_target.parameters()):
                         target_param.data.copy_(config["tau"] * param.data + (1 - config["tau"]) * target_param.data)
-
+        if episode % config["save_freq"] == 0:
+            torch.save({
+                "actor": actor.state_dict(),
+                "critic1": critic1.state_dict(),
+                "critic2": critic2.state_dict(),
+                "critic1_target": critic1_target.state_dict(),
+                "critic2_target": critic2_target.state_dict(),
+                "values_optimizer": values_optimizer.state_dict(),
+                "policy_optimizer": policy_optimizer.state_dict(),
+                "alpha_cont": alpha_cont.item(),
+                "alpha_disc": alpha_cont.item(),
+                "alpha_cont_optimizer": alpha_cont_optimizer.state_dict(),
+                "alpha_disc_optimizer": alpha_disc_optimizer.state_dict(),
+            }, f"{config['save_folder']}episode_{episode}.pth")
 if __name__ == "__main__":
     config = {
         "num_episodes": 100,
@@ -166,5 +201,9 @@ if __name__ == "__main__":
         "update_actor_freq": 2,
         "update_target_freq": 1,
         "tau": 0.005,
+        "save_freq": 100,
+        "save_folder": "models/",
+        "load_model": False,
+        "load_path": "models/episode_100.pth",
     }
     train(config)
